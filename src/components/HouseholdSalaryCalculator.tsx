08e21f9d5cca01b04c8a8cbe678b98d67c5acde3
Laszlo Simon <98slaci@gmail.com>
2024-04-07 14:32:16 +0200

    Fixed no member error

--- src/components/HouseholdSalaryCalculator.tsx
+++ src/components/HouseholdSalaryCalculator.tsx
@@ -2,17 +2,28 @@ import { ReactElement } from "react";
 import FamilyMemberTabs from "./FamilyMemberTabs/FamilyMemberTabs";
 import HouseholdSummary from "./HouseholdSummary/HouseholdSummary";
 import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
+import { useMemberContext } from "../hooks/useMemberContext";
 // import Modal from "./SalaryCalculator/components/Modal";
 
 const HouseholdSalaryCalculator = (): ReactElement => {
+  const { members } = useMemberContext();
+
   return (
     <main className="flex flex-col justify-center items-center w-full mt-24 sm:flex-col md:flex-col lg:flex-row">
       <div className="bg-zinc-200 rounded-lg relative mb-5 h-[40rem] w-[22rem] sm:w-[38rem] md:w-[45rem] lg:w-[35rem] lg:mb-3 lg:mr-5">
         <FamilyMemberTabs />
-        <SalaryCalculator />
+        {members.length ? (
+          <>
+            <SalaryCalculator />
+          </>
+        ) : (
+          <p className="flex justify-center items-center h-full text-2xl text-red-600">
+            Hozzon létre új személyt!
+          </p>
+        )}
       </div>
       <div className="bg-zinc-200 rounded-lg h-[40rem] w-[22rem] sm:w-[38rem] md:w-[45rem] lg:w-[35rem] mb-5 lg:mb-3">
-        <HouseholdSummary />
+        {members.length ? <HouseholdSummary /> : ""}
       </div>
 
       {/* <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50">
